4d4128d448d58dddb78ae1bef3333b19b81d6662
Danny Rodriguez <danny007111@hotmail.com>
2022-05-10 21:32:50 -0500

    Building code for API to work

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+let city = "Chicago";
+function getInfo() {
+    fetch(`https://goweather.herokuapp.com/weather/${city}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    });
+}
+
+getInfo();
